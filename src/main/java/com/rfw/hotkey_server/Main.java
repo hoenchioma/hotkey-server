235ecc86f99afd8441cd612a90575a64fe70ea06
Raheeb Hassan <raheeb998@gmail.com>
2019-09-09 21:11:02 +0600

    Initial commit

---
+++ src/main/java/com/rfw/hotkey_server/Main.java
@@ -0,0 +1,28 @@
+package com.rfw.hotkey_server;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+
+        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+        stage.setTitle("Hotkey Server");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/com/rfw/hotkey_server/SceneController.java
@@ -0,0 +1,9 @@
+package com.rfw.hotkey_server;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SceneController {
+
+    public Label label;
+}
