62e76aa3a953d2b38002eebacfdbc49d3b90f7f1
Raheeb Hassan <raheeb998@gmail.com>
2019-09-13 18:02:42 +0600

    Setup socket connection framework

---
+++ src/main/java/com/rfw/hotkey_server/net/Receiver.java
@@ -0,0 +1,45 @@
+package com.rfw.hotkey_server.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Receiver extends Thread {
+    private static final Logger LOGGER = Logger.getLogger(Receiver.class.getName());
+
+    private Server server;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private String clientName;
+
+    public volatile boolean stop = false;
+
+    Receiver(Socket socket, BufferedReader in, PrintWriter out, String clientName, Server server) {
+        this.server = server;
+        this.in = in;
+        this.out = out;
+        this.clientName = clientName;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        while (!stop && socket.isConnected()) {
+            try {
+                String line = in.readLine();
+                handleMessage(line);
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "Receiver.run: IO error closing connection");
+                break;
+            }
+        }
+    }
+
+    private void handleMessage(String message) {
+        // TODO: implement JSON parsing and message handling
+    }
+}
---
+++ src/main/java/com/rfw/hotkey_server/net/Server.java
@@ -0,0 +1,79 @@
+package com.rfw.hotkey_server.net;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Server {
+    private static final Logger LOGGER = Logger.getLogger(Server.class.getName());
+
+    private ServerSocket serverSocket;
+    public volatile boolean stop = false;
+
+    private Server(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    private void startServer() throws IOException {
+        System.out.println("Starting server on port " + serverSocket.getLocalPort() + " ...");
+        while (!stop) {
+            Socket socket = serverSocket.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            if (checkSocket(socket, in, out)) {
+                String clientName = handshake(socket, in, out);
+                if (clientName != null) {
+                    System.out.println("Connected to " + clientName);
+                    new Receiver(socket, in, out, clientName, this).start();
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if socket is valid client
+     * @return if socket is valid client
+     */
+    private boolean checkSocket(Socket socket, BufferedReader in, PrintWriter out) {
+        // TODO: implement check socket
+        return true;
+    }
+
+    /**
+     * Exchange device names with server desktop
+     * @return client device name if handshake was successful
+     */
+    private String handshake(Socket socket, BufferedReader in, PrintWriter out) {
+        try {
+            String clientName = in.readLine();
+            String deviceName = InetAddress.getLocalHost().getHostName();
+            out.println(deviceName);
+            return clientName;
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Server.handshake: Handshake failed closing connection");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            int port = 6969;
+            Server server = new Server(port);
+
+            try {
+                server.startServer();
+            } catch (IOException e) {
+                LOGGER.log(Level.SEVERE, "Server.main: IO exception occurred, server stopped");
+            }
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Server.main: Server failed to start");
+        }
+    }
+}
