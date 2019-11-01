729b6bbe5135ad970eb5a9a9e3f4fac9820ab07b
Raheeb Hassan <raheeb998@gmail.com>
2019-11-01 10:48:29 +0600

    renamed native lib to media-keys (from bridge) and added condition for loading lib in MediaKeys

--- src/main/java/com/rfw/hotkey_server/commands/MediaKeys.java
+++ src/main/java/com/rfw/hotkey_server/commands/MediaKeys.java
@@ -5,17 +5,29 @@ package com.rfw.hotkey_server.commands;
  */
 public class MediaKeys {
     static {
-        String libName = "bridge";
-        if (System.getProperty("os.name").contains("Windows")) {
-            if (System.getProperty("sun.arch.data.model").equals("32")) {
+        loadLibrary();
+    }
+
+    private static void loadLibrary() {
+        final String libName = "media-keys";
+        final String osName = System.getProperty("os.name").toLowerCase();
+        final String jreArch = System.getProperty("sun.arch.data.model");
+        if (osName.contains("win")) { // windows system
+            if (jreArch.equals("32")) { // 32 bit jre
                 System.loadLibrary(libName + "-x86");
-            } else if (System.getProperty("sun.arch.data.model").equals("64")) {
+            } else if (jreArch.equals("64")) { // 64 bit jre
                 System.loadLibrary(libName + "-x64");
             } else {
                 throw new IllegalStateException("Unknown JVM architecture");
             }
+        } else if (osName.contains("nix") | osName.contains("nux") | osName.contains("aix")) { // unix systems
+            if (jreArch.equals("32")) throw new IllegalStateException("32 bit JRE on Linux not supported");
+            System.loadLibrary(libName);
+        } else if (osName.contains("mac")) { // macOS system
+            throw new IllegalStateException("MacOS not supported");
+        } else {
+            throw new IllegalStateException("Unsupported OS");
         }
-        // TODO: linux
     }
 
     public static native void volumeMute();
@@ -29,9 +41,4 @@ public class MediaKeys {
     public static native void songNext();
 
     public static native void songPlayPause();
-
-    public static void main(String[] args) {
-        songPlayPause();
-        System.getProperties().list(System.out);
-    }
 }
