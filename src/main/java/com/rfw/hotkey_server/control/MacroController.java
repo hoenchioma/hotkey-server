b3c25d44aa21ebe355702072f8fcfcdf4e1825e9
Farhan Kabir <farhankabir24csedu012@gmail.com>
2019-11-20 19:53:43 +0600

    documented and added arrow keys to macros

--- src/main/java/com/rfw/hotkey_server/control/MacroController.java
+++ src/main/java/com/rfw/hotkey_server/control/MacroController.java
@@ -13,8 +13,13 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class MacroController
-{
+/**
+ * Controller for handling and executing macro key combinations.
+ *
+ * @author Farhan Kabir
+ */
+
+public class MacroController {
     private static final Logger LOGGER = Logger.getLogger(MacroController.class.getName());
 
     private Robot robot;
@@ -24,155 +29,7 @@ public class MacroController
     }
 
     public void handleIncomingPacket(JSONObject packet) {
-
-        pressKey(packet,0);
-        // ArrayList<String> keys = new ArrayList<>();
-        /*int totalKeys = packet.getInt("size");
-        for(int i = 0; i < totalKeys; i++) {
-            switch (packet.getString(Integer.toString(i))) {
-                case "A":
-                    keys.add("VK_A");
-                    break;
-                case "B":
-                    keys.add("VK_B");
-                    break;
-                case "C":
-                    keys.add("VK_C");
-                    break;
-                case "D":
-                    keys.add("VK_D");
-                    break;
-                case "E":
-                    keys.add("VK_E");
-                    break;
-                case "F":
-                    keys.add("VK_F");
-                    break;
-                case "G":
-                    keys.add("VK_G");
-                    break;
-                case "H":
-                    keys.add("VK_H");
-                    break;
-                case "I":
-                    keys.add("VK_I");
-                    break;
-                case "J":
-                    keys.add("VK_J");
-                    break;
-                case "K":
-                    keys.add("VK_K");
-                    break;
-                case "L":
-                    keys.add("VK_L");
-                    break;
-                case "M":
-                    keys.add("VK_M");
-                    break;
-                case "N":
-                    keys.add("VK_N");
-                    break;
-                case "O":
-                    keys.add("VK_O");
-                    break;
-                case "P":
-                    keys.add("VK_P");
-                    break;
-                case "Q":
-                    keys.add("VK_Q");
-                    break;
-                case "R":
-                    keys.add("VK_R");
-                    break;
-                case "S":
-                    keys.add("VK_S");
-                    break;
-                case "T":
-                    keys.add("VK_T");
-                    break;
-                case "U":
-                    keys.add("VK_U");
-                    break;
-                case "V":
-                    keys.add("VK_V");
-                    break;
-                case "W":
-                    keys.add("VK_W");
-                    break;
-                case "X":
-                    keys.add("VK_X");
-                    break;
-                case "Y":
-                    keys.add("VK_Y");
-                    break;
-                case "Z":
-                    keys.add("VK_Z");
-                    break;
-                case "0":
-                    keys.add("VK_0");
-                    break;
-                case "1":
-                    keys.add("VK_1");
-                    break;
-                case "2":
-                    keys.add("VK_2");
-                    break;
-                case "3":
-                    keys.add("VK_3");
-                    break;
-                case "4":
-                    keys.add("VK_4");
-                    break;
-                case "5":
-                    keys.add("VK_5");
-                    break;
-                case "6":
-                    keys.add("VK_6");
-                    break;
-                case "7":
-                    keys.add("VK_7");
-                    break;
-                case "8":
-                    keys.add("VK_8");
-                    break;
-                case "9":
-                    keys.add("VK_9");
-                    break;
-                case "ESC":
-                    keys.add("VK_ESCAPE");
-                    break;
-                case "END":
-                    keys.add("VK_END");
-                    break;
-                case "CTRL":
-                    keys.add("VK_CONTROL");
-                    break;
-                case "ALT":
-                    keys.add("VK_ALT");
-                    break;
-                case "SHIFT":
-                    keys.add("VK_SHIFT");
-                    break;
-                case "INS":
-                    keys.add("VK_INSERT");
-                    break;
-                case "DEL":
-                    keys.add("VK_DELETE");
-                    break;
-                case "HOME":
-                    keys.add("VK_HOME");
-                    break;
-                case "PGUP":
-                    keys.add("VK_PAGE_UP");
-                    break;
-                case "PGDN":
-                    keys.add("VK_PAGE_DOWN");
-                    break;
-            }
-
-        }
-        sendKeysCombo(keys);*/
-
+        pressKey(packet, 0);
     }
 
     public void pressKey(JSONObject packet, int currentKey) {
@@ -315,8 +172,17 @@ public class MacroController
                 case "PGUP":
                     robot.keyPress(KeyEvent.VK_PAGE_UP);
                     break;
-                case "PGDN":
-                    robot.keyPress(KeyEvent.VK_PAGE_DOWN);
+                case "UP":
+                    robot.keyPress(KeyEvent.VK_UP);
+                    break;
+                case "DOWN":
+                    robot.keyPress(KeyEvent.VK_DOWN);
+                    break;
+                case "LEFT":
+                    robot.keyPress(KeyEvent.VK_LEFT);
+                    break;
+                case "RIGHT":
+                    robot.keyPress(KeyEvent.VK_RIGHT);
                     break;
             }
             pressKey(packet, currentKey + 1);
@@ -459,31 +325,21 @@ public class MacroController
                 case "PGDN":
                     robot.keyRelease(KeyEvent.VK_PAGE_DOWN);
                     break;
+                case "UP":
+                    robot.keyRelease(KeyEvent.VK_UP);
+                    break;
+                case "DOWN":
+                    robot.keyRelease(KeyEvent.VK_DOWN);
+                    break;
+                case "LEFT":
+                    robot.keyRelease(KeyEvent.VK_LEFT);
+                    break;
+                case "RIGHT":
+                    robot.keyRelease(KeyEvent.VK_RIGHT);
+                    break;
             }
 
 
         }
     }
-    /*public static void sendKeysCombo(ArrayList keys) {
-        try {
-
-            Robot robot = new Robot();
-
-            Class<?> cl = KeyEvent.class;
-
-            int [] intKeys = new int [keys.size()];
-
-            for (int i = 0; i < keys.size(); i++) {
-                Field field = cl.getDeclaredField(keys.get(i).toString());
-                intKeys[i] = field.getInt(field);
-                robot.keyPress(intKeys[i]);
-            }
-
-            for (int i = keys.size() - 1; i >= 0; i--)
-                robot.keyRelease(intKeys[i]);
-        }
-        catch (Throwable e) {
-            System.err.println(e);
-        }
-    }*/
 }
